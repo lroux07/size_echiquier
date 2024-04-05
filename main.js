c9f9911f6b8a01790bf9d9f18edc1e8fceeee064
lroux07 <lroux07@edenschool.fr>
2024-04-05 13:51:54 +0200

    Create td and modulo

--- main.js
+++ main.js
@@ -4,4 +4,14 @@ let boardSize = 7;
 for(let i = 0; i < boardSize; i++) {
   const tr = document.createElement('tr');
   board.appendChild(tr);
+
+  for(let j = 0; j < boardSize; j++) {
+    const td = document.createElement('td');
+
+    if((i + j) % 2 === 0) {
+      td.classList.add('black');
+    }
+
+    tr.appendChild(td);
+  }
 }
